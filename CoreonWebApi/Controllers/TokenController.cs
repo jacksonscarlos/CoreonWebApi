48f4d5dee355c67c56bf22784600def4601c1dcf
jacksonscarlos <49418216+jacksonscarlos@users.noreply.github.com>
2019-04-09 15:01:24 -0300

    WebApi

---
+++ CoreonWebApi/Controllers/TokenController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreonWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreonWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TokenController : Controller
+    {
+        [HttpPost]
+        public ActionResult<string> Post([Required] string usuario, [Required] string senha)
+        {
+            return Ok("Autorizado");
+
+
+        }
+    }
+}
